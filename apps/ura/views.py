33968bbbc539b5bebc4b07a814a54e4b56197035
Raphael Kamashev <wizzzet@gmail.com>
2017-10-03 03:31:07 +0300

    getMoving API

--- apps/ura/views.py
+++ apps/ura/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
+
 from django.db.models import Q
+from django.template.defaultfilters import floatformat
 
 from base.utils import parse_float
 from base.exceptions import APIProcessError, ReportException
 from reports.utils import get_period, get_wialon_geozones_report_template_id, \
     cleanup_and_request_report, exec_report, get_report_rows, parse_wialon_report_datetime, \
-    parse_timedelta, format_timedelta
+    parse_timedelta, format_timedelta, utc_to_local_time
 from snippets.utils.datetime import utcnow
 from ura import models
 from ura.lib.resources import URAResource
@@ -14,6 +17,10 @@ from ura.utils import parse_datetime, get_organization_user, parse_xml_input_dat
 from ura.wialon.api import get_drivers_list, get_routes_list, get_units_list
 from ura.wialon.auth import authenticate_at_wialon
 from users.models import User
+
+
+def float_format(value, arg=0):
+    return floatformat(value, arg).replace(',', '.')
 
 
 class URADriversResource(URAResource):
@@ -310,6 +317,18 @@ class URABreakJobsResource(URAResource):
         return XMLResponse('ura/ackBreakJobs.xml', context)
 
 
+RIDES_GEOZONE_FROM_COL = 1
+RIDES_DATE_FROM_COL = 3
+RIDES_DATE_TO_COL = 4
+RIDES_DISTANCE_END_COL = 5
+RIDES_TIME_TOTAL_COL = 6
+RIDES_TIME_PARKING_COL = 7
+RIDES_FUEL_LEVEL_START_COL = 8
+RIDES_FUEL_LEVEL_END_COL = 9
+RIDES_ODOMETER_START_COL = 10
+RIDES_ODOMETER_END_COL = 11
+
+
 class URARacesResource(URAResource):
     model_mapping = {
         'date_begin': ('dateBegin', parse_datetime),
@@ -318,17 +337,6 @@ class URARacesResource(URAResource):
         'unit_id': ('idUnit', int),
         'route_id': ('idRoute', int)
     }
-
-    RIDES_GEOZONE_FROM_COL = 1
-    RIDES_DATE_FROM_COL = 3
-    RIDES_DATE_TO_COL = 4
-    RIDES_DISTANCE_END_COL = 5
-    RIDES_TIME_TOTAL_COL = 6
-    RIDES_TIME_PARKING_COL = 7
-    RIDES_FUEL_LEVEL_START_COL = 8
-    RIDES_FUEL_LEVEL_TO_COL = 9
-    RIDES_ODOMETER_START_COL = 10
-    RIDES_ODOMETER_TO_COL = 11
 
     @staticmethod
     def get_next_point(points, points_iterator=None):
@@ -373,6 +381,12 @@ class URARacesResource(URAResource):
                 return error_response(
                     'Заявка c ID=%s не найдена' % data['job_id'], code='job_not_found'
                 )
+
+            races = []
+            job_info = {
+                'obj': job,
+                'races': races
+            }
 
             unit_id = int(data.get('unit_id', job.unit_id))
             route_id = int(data.get('route_id', job.route_id))
@@ -431,12 +445,6 @@ class URARacesResource(URAResource):
                         'Не удалось извлечь данные о поездке', code='wialon_geozones_rows_error'
                     )
 
-            races = []
-            job_info = {
-                'obj': job,
-                'races': races
-            }
-
             points = routes_dict[route_id]['points']
             if len(points) < 2:
                 return error_response(
@@ -452,13 +460,21 @@ class URARacesResource(URAResource):
                 'points': []
             }
 
+            last_distance = .0
+
             for row in report_data['unit_rides']:
                 row_data = row['c']
-                row_point_name = row_data[self.RIDES_GEOZONE_FROM_COL].strip()
+                row_point_name = row_data[RIDES_GEOZONE_FROM_COL].strip()
 
                 if row_point_name == current_point:
-                    time_in = parse_wialon_report_datetime(row_data[self.RIDES_DATE_FROM_COL]['t'])
-                    time_out = parse_wialon_report_datetime(row_data[self.RIDES_DATE_TO_COL]['t'])
+                    time_in = utc_to_local_time(
+                        parse_wialon_report_datetime(row_data[RIDES_DATE_FROM_COL]['t']),
+                        request.user.ura_tz
+                    )
+                    time_out = utc_to_local_time(
+                        parse_wialon_report_datetime(row_data[RIDES_DATE_TO_COL]['t']),
+                        request.user.ura_tz
+                    )
 
                     if race['date_start'] is None:
                         race['date_start'] = time_in
@@ -466,38 +482,40 @@ class URARacesResource(URAResource):
                     point_info = {
                         'time_in': time_in,
                         'time_out': time_out,
-                        'params': {}
+                        'params': OrderedDict()
                     }
 
+                    current_distance = parse_float(
+                        row_data[RIDES_DISTANCE_END_COL]
+                    )
+
+                    last_distance += current_distance
+                    distance_delta = float_format(last_distance, -2)
+
                     if row_point_name == start_point:
-                        point_info['type'] = 'start_point'
+                        point_info['type'] = 'startPoint'
                         point_info['params']['fuelLevel'] = parse_float(
-                            row_data[self.RIDES_FUEL_LEVEL_START_COL]
+                            row_data[RIDES_FUEL_LEVEL_START_COL]
                         )
-                        point_info['params']['distance'] = parse_float(
-                            row_data[self.RIDES_DISTANCE_END_COL]
-                        )
+
+                        point_info['params']['distance'] = distance_delta
 
                     elif row_point_name == end_point:
-                        point_info['type'] = 'end_point'
+                        point_info['type'] = 'endPoint'
                         point_info['params']['fuelLevel'] = parse_float(
-                            row_data[self.RIDES_FUEL_LEVEL_TO_COL]
+                            row_data[RIDES_FUEL_LEVEL_END_COL]
                         )
-                        point_info['params']['distance'] = parse_float(
-                            row_data[self.RIDES_DISTANCE_END_COL]
-                        )
+                        point_info['params']['distance'] = distance_delta
 
                     else:
-                        point_info['type'] = 'check_point'
+                        point_info['type'] = 'checkPoint'
                         point_info['params']['fuelLevelIn'] = parse_float(
-                            row_data[self.RIDES_FUEL_LEVEL_START_COL]
+                            row_data[RIDES_FUEL_LEVEL_START_COL]
                         )
-                        point_info['params']['distanceIn'] = parse_float(
-                            row_data[self.RIDES_DISTANCE_END_COL]
-                        )
+                        point_info['params']['distanceIn'] = distance_delta
 
-                        time_total = parse_timedelta(row_data[self.RIDES_TIME_TOTAL_COL])
-                        time_parking = parse_timedelta(row_data[self.RIDES_TIME_PARKING_COL])
+                        time_total = parse_timedelta(row_data[RIDES_TIME_TOTAL_COL]).seconds
+                        time_parking = parse_timedelta(row_data[RIDES_TIME_PARKING_COL]).seconds
                         point_info['params']['moveTime'] = format_timedelta(
                             max(0, time_total - time_parking)
                         )
@@ -509,6 +527,7 @@ class URARacesResource(URAResource):
                     )
 
                     if new_loop:
+                        last_distance = .0
                         if race['date_end'] is None:
                             race['date_end'] = time_out
 
@@ -525,6 +544,12 @@ class URARacesResource(URAResource):
 
 
 class URAMovingResource(URAResource):
+    model_mapping = {
+        'date_begin': ('dateBegin', parse_datetime),
+        'date_end': ('dateEnd', parse_datetime),
+        'unit_id': ('idUnit', int),
+    }
+
     def post(self, request, *args, **kwargs):
         units = []
 
@@ -534,12 +559,15 @@ class URAMovingResource(URAResource):
             'units': units
         })
 
+        sess_id = authenticate_at_wialon(request.user.wialon_token)
+        routes_list = get_routes_list(sess_id=sess_id, get_points=True)
+        routes_dict = {x['id']: x for x in routes_list}
+
         units_els = request.data.xpath('/getMoving/unit')
 
         if not units_els:
             return error_response(
-                'Не указаны объекты типа unit',
-                code='units_not_found'
+                'Не указаны объекты типа unit', code='units_not_found'
             )
 
         template_id = request.user.wialon_geozones_report_template_id
@@ -549,7 +577,114 @@ class URAMovingResource(URAResource):
                 code='geozones_report_not_found'
             )
 
-        # for unit_el in units_els:
-        #     pass
+        for unit_el in units_els:
+            data = parse_xml_input_data(request, self.model_mapping, unit_el)
+
+            unit_id = int(data.get('unit_id', data['unit_id']))
+
+            unit_info = {
+                'id': unit_id,
+                'date_begin': None,
+                'date_end': None,
+                'points': []
+            }
+
+            dt_from, dt_to = get_period(
+                data['date_begin'],
+                data['date_end'],
+                request.user.ura_tz
+            )
+
+            cleanup_and_request_report(
+                request.user,
+                get_wialon_geozones_report_template_id(request.user),
+                item_id=unit_id,
+                sess_id=sess_id,
+            )
+
+            try:
+                r = exec_report(
+                    request.user,
+                    get_wialon_geozones_report_template_id(request.user),
+                    dt_from,
+                    dt_to,
+                    object_id=unit_id,
+                    sess_id=sess_id
+                )
+            except ReportException:
+                raise APIProcessError(
+                    'Не удалось получить отчет о поездках', code='wialon_geozones_report_error'
+                )
+
+            report_data = {
+                'unit_fillings': [],
+                'unit_engine_hours': [],
+                'unit_rides': [],
+                'unit_thefts': []
+            }
+
+            for table_index, table_info in enumerate(r['reportResult']['tables']):
+                try:
+                    rows = get_report_rows(
+                        request.user,
+                        table_index,
+                        table_info,
+                        level=1,
+                        sess_id=sess_id
+                    )
+
+                    report_data[table_info['name']] = rows
+
+                except ReportException:
+                    raise APIProcessError(
+                        'Не удалось извлечь данные о поездке', code='wialon_geozones_rows_error'
+                    )
+
+            time_out = None
+
+            for row in report_data['unit_rides']:
+                row_data = row['c']
+                time_in = utc_to_local_time(
+                    parse_wialon_report_datetime(row_data[RIDES_DATE_FROM_COL]['t']),
+                    request.user.ura_tz
+                )
+                time_out = utc_to_local_time(
+                    parse_wialon_report_datetime(row_data[RIDES_DATE_TO_COL]['t']),
+                    request.user.ura_tz
+                )
+
+                if unit_info['date_begin'] is None:
+                    unit_info['date_begin'] = time_in
+
+                time_total = parse_timedelta(row_data[RIDES_TIME_TOTAL_COL]).seconds
+                time_parking = parse_timedelta(row_data[RIDES_TIME_PARKING_COL]).seconds
+                move_time = time_total - time_parking
+
+                point_info = {
+                    # TODO: SPACE
+                    'name': row_data[RIDES_GEOZONE_FROM_COL],
+                    'time_in': time_in,
+                    'time_out': time_out,
+                    'params': OrderedDict((
+                        ('startFuelLevel', parse_float(row_data[RIDES_FUEL_LEVEL_START_COL])),
+                        ('endFuelLevel', parse_float(row_data[RIDES_FUEL_LEVEL_END_COL])),
+                        ('fuelRefill', ''),
+                        ('fuelDrain', ''),
+                        ('stopMinutes', row_data[RIDES_TIME_PARKING_COL]),
+                        ('moveMinutes', format_timedelta(max(0, move_time))),
+                        ('motoHours', ''),
+                        (
+                            'odometer',
+                            float_format(parse_float(row_data[RIDES_DISTANCE_END_COL]), -2)
+                        )
+                    ))
+                }
+
+                unit_info['points'].append(point_info)
+
+            if time_out is not None:
+                unit_info['date_end'] = time_out
+
+            units.append(unit_info)
 
         return XMLResponse('ura/moving.xml', context)

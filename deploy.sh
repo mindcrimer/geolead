d5eb645a2fb730e8ca405383237fd6fa7e720220
Raphael Kamashev <wizzzet@gmail.com>
2017-10-25 13:50:22 +0300

    config

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ find . -type f -name "*.pyc" -exec rm -f {} \;
 pip install -r requirements.txt
 yes "yes" | python manage.py migrate
 python manage.py collectstatic --noinput
-supervisorctl restart geolead
+supervisorctl restart geolead_resource

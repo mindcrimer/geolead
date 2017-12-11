c91dbf9dd36d91124752e56f39aae7b7b0c0314e
Raphael Kamashev <wizzzet@gmail.com>
2017-12-11 12:50:20 +0300

    бланки отчетов

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 
 from reports.views.discharge import DischargeView
 from reports.views.drivingstyle import DrivingStyleView
+from reports.views.finished_jobs import FinishedJobsView
+from reports.views.invalid_job_start_end import InvalidJobStartEndView
 from reports.views.malfunctions import MalfunctionsView
+from reports.views.overstatements import OverstatementsView
+
 
 urlpatterns = (
     url(
@@ -15,7 +19,19 @@ urlpatterns = (
         DrivingStyleView.as_view(), name='driving_style'
     ),
     url(
+        r'^finished-jobs/$',
+        FinishedJobsView.as_view(), name='finished_jobs'
+    ),
+    url(
+        r'^invalid-job-start-end/$',
+        InvalidJobStartEndView.as_view(), name='invalid_job_start_end'
+    ),
+    url(
         r'^malfunctions/$',
         MalfunctionsView.as_view(), name='malfunctions'
+    ),
+    url(
+        r'^overstatements/$',
+        OverstatementsView.as_view(), name='overstatements'
     )
 )
---
+++ apps/reports/views/invalid_job_start_end.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from collections import OrderedDict
+import datetime
+
+from base.exceptions import ReportException
+from reports import forms
+from reports.utils import get_period
+from reports.views.base import BaseReportView, WIALON_NOT_LOGINED, WIALON_USER_NOT_FOUND
+from users.models import User
+
+
+class InvalidJobStartEndView(BaseReportView):
+    """Отчет о несвоевременном начале и окончании выполнения задания"""
+    form = forms.DrivingStyleForm
+    template_name = 'reports/invalid_job_start_end.html'
+    report_name = 'Отчет о несвоевременном начале и окончании выполнения задания'
+
+    @staticmethod
+    def get_new_grouping():
+        return {
+            'key': '',
+            'place': '',
+            'dt': ''
+        }
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(InvalidJobStartEndView, self).get_context_data(**kwargs)
+        report_data = None
+        form = kwargs['form']
+        kwargs['today'] = datetime.date.today()
+        stats = {
+            'total': 0
+        }
+
+        if self.request.POST:
+            report_data = OrderedDict()
+
+            if form.is_valid():
+                sess_id = self.request.session.get('sid')
+                if not sess_id:
+                    raise ReportException(WIALON_NOT_LOGINED)
+
+                user = User.objects.filter(is_active=True) \
+                    .filter(wialon_username=self.request.session.get('user')).first()
+
+                if not user:
+                    raise ReportException(WIALON_USER_NOT_FOUND)
+
+                dt_from, dt_to = get_period(
+                    form.cleaned_data['dt_from'],
+                    form.cleaned_data['dt_to'],
+                    user.wialon_tz
+                )
+
+        kwargs.update(
+            stats=stats,
+            report_data=report_data
+        )
+
+        return kwargs

8e7648975d00e8f63b138c7c6401ffb17fd9c919
Raphael Kamashev <wizzzet@gmail.com>
2018-03-23 14:19:15 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 git pull
 source venv/bin/activate
 find . -type f -name "*.pyc" -exec rm -f {} \;
+pip install -U pip setuptools pip-tools
 pip install -r requirements.txt
 yes "yes" | python manage.py migrate
 cd static/
